651385fa8e3a16edb57532a709a6ee4f2b28a0fa
Goblin Oats <goblin@tonk.gg>
2023-09-27 19:46:46 +0100

    feat: Debug Tower and Tonk item game setup

--- DebugusTower/DebugusTower.js
+++ DebugusTower/DebugusTower.js
@@ -1,40 +1,92 @@
 import ds from 'downstream';
 
-let UPDATE_COCKTAIL_MS = 100000;
-let lastUpdated = Date.now();
-let cocktail;
+let game, players;
 
-async function getCocktailOfTheMoment() {
-    const now = Date.now();
-    const recentlyUpdate = now - lastUpdated < UPDATE_COCKTAIL_MS;
-    if (!cocktail || !recentlyUpdate) {
-        lastUpdated = now;
-        cocktail = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
-            .then(res => res.json());
+let wants_to_join = false;
+
+async function getGame() {
+    try {
+        let response = await fetch("http://localhost:8082/game");
+        let raw = await response.text();
+        return JSON.parse(raw);
+    } catch (e) {
+        console.log(e);
+        return ({
+            status: "GameServerDown"
+        })
     }
-    if (!cocktail) {
-        return 'A Mystery';
-    }
-    const drink = cocktail.drinks.find(() => true);
-    const ingriedients = [
-        drink.strIngredient1,
-        drink.strIngredient2,
-        drink.strIngredient3,
-        drink.strIngredient4,
-        drink.strIngredient5,
-    ].filter(ing => !!ing);
-    return `${drink.strDrink}: ${ingriedients.join(', ')}`;
 }
 
-export default async function update({ selected }) {
+async function getPlayers(gameId) {
+    // fetch("localhost:8082/game/9d19e34892d546bea2fbeba08be9e573/player", requestOptions)
+    // .then(response => response.text())
+    // .then(result => console.log(result))
+    // .catch(error => console.log('error', error));
+
+    try {
+        let response = await fetch(`http://localhost:8082/game/${gameId}/player`);
+        let raw = await response.text();
+        return JSON.parse(raw);
+    } catch (e) {
+        console.log(e);
+        return (`[]`)
+    }
+}
+
+async function requestJoin(gameId, playerId, secretKey) {
+    // var myHeaders = new Headers();
+    // myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "id": playerId,
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Sec-Fetch-Mode": "cors"
+        },
+        mode: "cors",
+        body: raw,
+    };
+
+    try {
+        let response = await fetch(`http://localhost:8082/game/${gameId}/player`, requestOptions)
+        let text = await response.text();
+        console.log(text);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default async function update(params) {
+    const { selected, player } = params; 
+    console.log(params);
     const { tiles, mobileUnit } = selected || {};
     const selectedTile = tiles && tiles.length === 1 ? tiles[0] : undefined;
     const selectedBuilding = selectedTile && selectedTile.building ? selectedTile.building : undefined;
     const selectedEngineer = mobileUnit;
     const inputBag = selectedBuilding && selectedBuilding.bags.find(b => b.key == 0).bag;
-    const canPourDrink = inputBag && inputBag.slots.length == 2 && inputBag.slots.every(slot => slot.balance > 0) && selectedEngineer;
+    const canCraftTonk = inputBag && inputBag.slots.length == 2 && inputBag.slots.every(slot => slot.balance > 0) && selectedEngineer;
+    // console.log("building id: ", selectedBuilding.id);
+    // console.log(player);
 
-    const cocktail = await getCocktailOfTheMoment();
+    game = await getGame();
+    players = await getPlayers(game.id);
+
+
+    if (wants_to_join) {
+        try {
+            await requestJoin(game.id, player.id)
+            wants_to_join = false;
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    const joinGame = () => {
+        wants_to_join = true;
+    }
 
     const craft = () => {
         if (!selectedEngineer) {
@@ -56,28 +108,40 @@ export default async function update({ selected }) {
         ds.log('Enjoy your drink!');
     };
 
+    const has_tonk = player.mobileUnits[0].bags[0].bag.slots.findIndex(b => b.item && b.item.name.value == 'Tonk') >= 0 || player.mobileUnits[0].bags[1].bag.slots.findIndex(b => b.item && b.item.name.value == 'Tonk') >= 0
+    const player_is_in_game = players.findIndex(p => p.id == player.id) >= 0;
+
+    let html =` 
+            <p> Number of players waiting in the lobby: ${players.length} </p>
+            <br/>
+            ${players.length < 3 ? `<p>Game can start when ${3 - players.length} more players join the lobby</p>`: ""}
+            <hr />
+        `
+    let buttons = []
+    if (!player_is_in_game && !!has_tonk) {
+        buttons.push({ text: 'Join Game', type: 'action', action: joinGame, disabled: !!!has_tonk });
+    }
+    if (!has_tonk) {
+        buttons.push({ text: 'Craft Tonk', type: 'action', action: craft, disabled: !canCraftTonk })
+        html+=`
+            <p> You need a Tonk to play the game </p>
+            <br />
+        `;
+    }
+
+
     return {
         version: 1,
         components: [
             {
                 id: 'debugus-tower',
                 type: 'building',
-                content: [
-                    {
-                        id: 'default',
-                        type: 'inline',
-                        buttons: [ { text: 'Pour Cocktail', type: 'action', action: craft, disabled: !canPourDrink } ],
-                        html: `
-                            <p>We supply the best drinks in Hexwood!</p>
-                            <br />
-                            <p>The current cocktail is:</p>
-                            <br />
-                            <strong>${cocktail}</strong>
-                            <br />
-                            <br />
-                        `
-                    },
-                ],
+                content: [{
+                    id: 'default',
+                    type: 'inline',
+                    html: html,
+                    buttons: buttons
+                }],
             },
         ],
     };
--- DebugusTower/Tonk.js
+++ DebugusTower/Tonk.js
@@ -1,9 +1,79 @@
 import ds from 'downstream';
 
-let numSips = 0;
+var game, tonkPlayer;
+async function getGame() {
+    try {
+        let response = await fetch("http://localhost:8082/game");
+        let raw = await response.text();
+        return JSON.parse(raw);
+    } catch (e) {
+        console.log(e);
+        return (`{ "status": "GameServerDown" }`)
+    }
+}
 
-export default async function update({ selected }) {
+async function getPlayer(id) {
+    try {
+        let response = await fetch(`http://localhost:8082/player/${id}`)
+        let raw = await response.text();
+        return JSON.parse(raw);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function isInGame(gameId, playerId) {
+    try {
+        let response = await fetch(`http://localhost:8082/game/${gameId}/player`);
+        let raw = await response.text();
+        let players = JSON.parse(raw);
+        return players.findIndex((p) => p.id == playerId) !== -1;
+    } catch (e) {
+
+    }
+}
+
+async function registerPlayer(id, hash, secret) {
+    var requestOptions = {
+        method: 'POST',
+      };
+      
+      try {
+        // let response = await fetch(`http://localhost:8082/player/${id}?secret_key=${secret}&onchain_hash=${hash}`, requestOptions)
+        let response = await fetch(`http://localhost:8082/player/${id}`, requestOptions)
+        // let text = await response.text();
+      } catch (e) {
+        console.log(e);
+      }
+}
+
+function formatHtml(status) {
+    if (status == "SPECTATOR") {
+        return `
+            <p> Please go to the debug us tower to join the game </p>
+        `
+    } else if (status == "Lobby") {
+        return `
+            <h3>${status}</h3>
+            <p> Waiting for the game to start... </p>
+        `
+    } else {
+        return "";
+    } 
+}
+
+export default async function update({ selected, player }) {
     const { mobileUnit } = selected || {};
+
+    game = await getGame();
+    tonkPlayer = await getPlayer(player.id);
+
+    if (tonkPlayer.id == "") {
+        await registerPlayer(player.id);
+    }
+
+    let has_joined = await isInGame(game.id, player.id);
+    let status = has_joined ? game.status : "SPECTATOR"
 
     const action = () => {
         // ds.dispatch(
@@ -27,15 +97,17 @@ export default async function update({ selected }) {
                         id: 'default',
                         type: 'inline',
                         html: `
-                            <p>Cocktail sips: ${numSips}</p>
+                            <h1> Debug Us </h1>
+                            <br/>
+                            ${formatHtml(status)}
                         `,
-                        buttons: [
-                            {
-                                text: 'Sip',
-                                type: 'action',
-                                action,
-                            },
-                        ],
+                        // buttons: [
+                        //     {
+                        //         text: 'Sip',
+                        //         type: 'action',
+                        //         action,
+                        //     },
+                        // ],
                     },
                 ],
             },
